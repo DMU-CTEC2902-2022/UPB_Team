c80a9f17968447d3f04ce4862c867d486f067629
Katbravo <98750940+Katbravo@users.noreply.github.com>
2022-04-29 14:30:50 +0100

    Added register controller

---
+++ Assignment2/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Controllers;
+
+namespace Assignment2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
